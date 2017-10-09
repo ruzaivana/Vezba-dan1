c2f1113143d07330cd2caa6dc622465fa995c818
ruzaivana <ruzaivana@gmail.com>
2017-10-09 17:09:52 +0200

    vezbe 6dan

---
+++ elements/vezba7.js
@@ -0,0 +1,45 @@
+
+
+var month = 14;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("Marth");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Avgust");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+        case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Input must be number btw 1 n 12");
+        break;
+}
---
+++ elements/vezba8.js
@@ -0,0 +1,2 @@
+var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Suterday", "Sunday"];
+console.log(weekday [6]);
